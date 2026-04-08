593279465e873e03fc650c4e66b20493676d8ce8
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2026-04-07 22:08:44 -0500

    Add'l print for redirector verbosity

--- brewer-remote-inclusive.py
+++ brewer-remote-inclusive.py
@@ -126,6 +126,8 @@ def main():
                     pass
                 else:
                     file_name = aliases[ixrd] + options.infile
+            else:
+                coffea_console.print(f"Skipping redirector prepending for file {file_name}")
 
             if options.copyInput:
                 if local_file_name is None and file_name.startswith("root://"):
