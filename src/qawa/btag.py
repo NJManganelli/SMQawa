dc0e2d873f9893dc1203bd52143c4c50fc1c4935
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2026-06-23 01:20:28 +0200

    Completely refactor btag class, dynamic WP loading from correctionlib, updated taggers, lazy efficiency loading to permit no-SF processing chain

--- src/qawa/btag.py
+++ src/qawa/btag.py
@@ -3,6 +3,7 @@ import pickle
 import hist
 import gzip
 import os
+from pathlib import Path
 from hist.intervals import clopper_pearson_interval
 
 from coffea.lookup_tools import dense_lookup
@@ -14,46 +15,68 @@ import awkward as ak
 import numpy as np
 import uproot
 
-
-def btag_id(wp:str='L', era:str='2018'):
-    # using deepjet
-    # ref : https://twiki.cern.ch/twiki/bin/view/CMS/BtagRecommendation
-    dict_wp = {
-            "2016"   : {"L": 0.0508, "M": 0.2598, "T": 0.6502},
-            "2016APV": {"L": 0.0480, "M": 0.2489, "T": 0.6377},
-            "2017"   : {"L": 0.0532, "M": 0.3040, "T": 0.7476},
-            "2018"   : {"L": 0.0490, "M": 0.2783, "T": 0.7100}
+def btag_disc_wps(tagger:str='deepJet'):
+    _disc_wps = {
+        "deepJet": {
+            "discriminant": "btagDeepFlavB",
+            "workingpoints": {
+                "2016"   : {"L": 0.0508, "M": 0.2598, "T": 0.6502},
+                "2016APV": {"L": 0.0480, "M": 0.2489, "T": 0.6377},
+                "2017"   : {"L": 0.0532, "M": 0.3040, "T": 0.7476},
+                "2018"   : {"L": 0.0490, "M": 0.2783, "T": 0.7100}
+            },
+        },
+        "UParTAK4": {
+            "discriminant": "btagUParTAK4B",
+            "workingpoints": None # dynamically loaded from btagging.json.gz instead
+        },
     }
-    return dict_wp[era][wp]
-
+    return _disc_wps[tagger]
+    
 class BTVCorrector:
-    def __init__(self, era:str='2018', wp:str='L', tagger:str='deepJet', isAPV=False):
+    def __init__(self, era:str='2018', wp:str='L', tagger:str='deepJet', isAPV=False, isEE=False, isBPix=False, clibhandler=None):
+        assert tagger in ["deepJet", "UParTAK4"]
+        assert wp in ["L", "M", "T", "XT", "XXT"]
         self._era = era
         self._wp  = wp
         self.isAPV = isAPV
+        self.isEE = isEE
+        self.isBPix = isBPix
         self._tagger = tagger
+        self._disc = btag_disc_wps(tagger=tagger)["discriminant"]
         self.eff_hist = None
-        _data_path = os.path.join(os.path.dirname(__file__), 'data')
-        with gzip.open(
-                f"{_data_path}/btv/{era+'_APV' if isAPV else era}_UL"
-                f"/eff-btag-{era+'_APV' if isAPV else era}.pkl.gz", 
-                'rb') as ifile:
-            self.eff_hist = pickle.loads(ifile.read())
+        _data_path = Path(os.path.dirname(__file__)) / 'data'
+        self._erasubera = era
+        if isAPV:
+            self._erasubera += "_APV"
+        elif isEE:
+            self._erasubera += "_EE"
+        elif isBPix:
+            self._erasubera += "_BPix"
+        self._erasuberanousc = self._erasubera.replace("_", "")
+        eff_path = Path(f"{_data_path}/btv/{self._erasubera}_UL/eff-btag-{self._erasubera}.pkl.gz") # Run 2 compatible version
+        if eff_path.exists():
+            with gzip.open(eff_path, 'rb') as ifile:
+                self.eff_hist = pickle.loads(ifile.read())
+    
+            b_tag = self.eff_hist[{'tagger':self._tagger, 'WP': wp, 'passWP': 1  }].values()
+            b_all = self.eff_hist[{'tagger':self._tagger, 'WP': wp, 'passWP': sum}].values()
+    
+            nom = b_tag / np.maximum(b_all, 1.)
+            dw, up = clopper_pearson_interval(b_tag, b_all)
+    
+            self.eff        = dense_lookup.dense_lookup(nom,[ax.edges for ax in self.eff_hist.axes[3:]])
+            self.eff_statUp = dense_lookup.dense_lookup(up ,[ax.edges for ax in self.eff_hist.axes[3:]])
+            self.eff_statDw = dense_lookup.dense_lookup(dw ,[ax.edges for ax in self.eff_hist.axes[3:]])
 
-        b_tag = self.eff_hist[{'tagger':self._tagger, 'WP': wp, 'passWP': 1  }].values()
-        b_all = self.eff_hist[{'tagger':self._tagger, 'WP': wp, 'passWP': sum}].values()
-
-        nom = b_tag / np.maximum(b_all, 1.)
-        dw, up = clopper_pearson_interval(b_tag, b_all)
-
-        self.eff        = dense_lookup.dense_lookup(nom,[ax.edges for ax in self.eff_hist.axes[3:]])
-        self.eff_statUp = dense_lookup.dense_lookup(up ,[ax.edges for ax in self.eff_hist.axes[3:]])
-        self.eff_statDw = dense_lookup.dense_lookup(dw ,[ax.edges for ax in self.eff_hist.axes[3:]])
-
-        self.clib = correctionlib.CorrectionSet.from_file(
-                f"{_data_path}/btv/{era+'_APV' if isAPV else era}_UL/"
+        # FIXME This will need better correctionlib handling in the future, akin to __init_legacy__ vs __init_clib__ in other functions perhaps
+        if clibhandler is not None:
+            self.clib = clibhandler.getCorrectionSet("btagging")
+        else:
+            self.clib = correctionlib.CorrectionSet.from_file(
+                f"{_data_path}/btv/{self._erasubera}_UL/"
                 f"btagging.json.gz"
-        )
+            )
 
         
     def lightSF(self, jet, syst="central"):
@@ -90,13 +113,28 @@ class BTVCorrector:
         untagged_sf = ak.prod(((1 - sf*eff) / (1 - eff))[~b_tagged], axis=-1)
         return ak.fill_none(tagged_sf * untagged_sf, 1.)
 
-    def append_btag_sf(self, jets: ak.Array, weights: Weights):
-        li_jets = jets[(jets.hadronFlavour==0) & (np.abs(jets.eta) <= 2.4)]
-        bc_jets = jets[(jets.hadronFlavour!=0) & (np.abs(jets.eta) <= 2.4)]
+    def tagged_jets(self, jets: ak.Array, wp=None):
+        # permits possible multi-WP expansion in the future
+        if wp is None:
+            wps = [self._wp]
+        elif isinstance(wp, str):
+            wps = [wp]
+        elif isinstance(wp, list):
+            wps = wp
+        else:
+            raise ValueError(f"Unhandled tagged_jets logic: self._wp={self._wp} ; wp={wp} ; wps={wps}")
+        return {wp: (getattr(jets, self._disc)  > self.clib[f"{self._tagger}_wp_values"].evaluate(wp)) for wp in wps}
 
-        wp_era_str = self._era + 'APV' if self.isAPV else self._era
-        b_tagged_li = (li_jets.btagDeepFlavB  > btag_id(wp = self._wp, era=wp_era_str))
-        b_tagged_bc = (bc_jets.btagDeepFlavB  > btag_id(wp = self._wp, era=wp_era_str))
+    def append_btag_sf(self, jets: ak.Array, weights: Weights):
+        if not hasattr(self, "eff"):
+            raise ValueError("The efficiency lookups were not loaded for the BTVCorrector, possibly due to lack of correctionlib implementation or failure to find the path")
+        # Eventually we may be able to migrate to a smarter compound lookup: https://cms-talk.web.cern.ch/t/suggestion-on-the-scope-of-provided-correctionlib-files/141772/4
+        eta_max = 2.4 if "2016" in self._era else 2.5
+        li_jets = jets[(jets.hadronFlavour==0) & (np.abs(jets.eta) <= eta_max)]
+        bc_jets = jets[(jets.hadronFlavour!=0) & (np.abs(jets.eta) <= eta_max)]
+        
+        b_tagged_li = (getattr(li_jets, self._disc)  > self.clib[f"{self._tagger}_wp_values"].evaluate(self._wp))
+        b_tagged_bc = (getattr(bc_jets, self._disc)  > self.clib[f"{self._tagger}_wp_values"].evaluate(self._wp))
 
         eff_li_nom = self.eff(li_jets.hadronFlavour, li_jets.pt, np.abs(li_jets.eta))
         eff_bc_nom = self.eff(bc_jets.hadronFlavour, bc_jets.pt, np.abs(bc_jets.eta))
@@ -130,15 +168,15 @@ class BTVCorrector:
             ) / (sf_nom_li*sf_nom_bc)
         )
 
-        # uncorrelated unceertainties
+        # uncorrelated uncertainties
         weights.add(
-            f'btag_sf_light_{self._era + "APV" if self.isAPV else self._era}',
+            f'btag_sf_light_{self._erasuberanousc}',
             np.ones(len(sf_nom)),
             weightUp  = self.combine(eff_li_nom, self.lightSF(li_jets, 'up'), b_tagged_li),
             weightDown= self.combine(eff_li_nom, self.lightSF(li_jets, 'down'), b_tagged_li)
         )
         weights.add(
-            f'btag_sf_bc_{self._era + "APV" if self.isAPV else self._era}',
+            f'btag_sf_bc_{self._erasuberanousc}',
             np.ones(len(sf_nom)),
             weightUp  = self.combine(eff_bc_nom, self.btagSF(bc_jets, 'up'), b_tagged_bc),
             weightDown= self.combine(eff_bc_nom, self.btagSF(bc_jets, 'down'), b_tagged_bc)
